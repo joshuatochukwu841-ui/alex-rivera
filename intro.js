5b7d2035efc1b936fe7efc4967b1e0556e3f6f3e
Okafor Tochukwu <joshuatochukwu841@gmail.com>
2026-07-14 17:14:39 +0100

    Modified my nav toggler btn

---
+++ intro.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById('menu-btn');
+const navbarMenu = document.getElementById('navbar-menu');
+
+menuBtn.addEventListener('click', ()=> {
+
+        navbarMenu.classList.toggle('displayNav')
+
+})
